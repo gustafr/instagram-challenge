271a0378edc9d0bedbd129c5abac8df3c6aecb9d
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-10-25 10:19:23 +0100

    first passing feature test for posts/new

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,10 +1,28 @@
 require 'rails_helper'
 
 describe 'posts' do
+
+  before do
+    visit ('/posts')
+  end
+
   context 'no posts have been uploaded yet' do
+
     scenario 'it should display a message that there are no posts' do
-      visit ('/posts')
       expect(page).to have_content "There are no posts in the system."
     end
+
+    scenario 'it should display add new post message' do
+      expect(page).to have_content "Add new post"
+    end
+  end
+
+  context 'User click on Add new post' do
+    scenario 'It shows an add posts form' do
+      click_link "Add new post"
+      expect(page.current_path).to eq ('/posts/new')
+
+    end
+
   end
 end
