a370052d591e47adc8682c6273bf83b173c0fe6b
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2015-10-28 12:23:10 +0100

    fixed error with migration posts to like

--- db/migrate/20151028112012_add_post_to_likes.rb
+++ db/migrate/20151028112012_add_post_to_likes.rb
@@ -1,5 +1,5 @@
 class AddPostToLikes < ActiveRecord::Migration
   def change
-    add_reference :likes, :user, index: true, foreign_key: true
+    add_reference :likes, :post, index: true, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028105040) do
+ActiveRecord::Schema.define(version: 20151028112012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,12 @@ ActiveRecord::Schema.define(version: 20151028105040) do
     t.boolean  "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "post_id"
   end
+
+  add_index "likes", ["post_id"], name: "index_likes_on_post_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -67,5 +72,7 @@ ActiveRecord::Schema.define(version: 20151028105040) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
